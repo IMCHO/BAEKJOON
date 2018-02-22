e665241c7d1b7f9ef1deca6580bc826782aa0dc8
인택 조 <dlsxor21c@naver.com>
2018-02-22 14:40:11 +0900

    Solve limit loop!

--- STEP_4/STEP_4/1110.py
+++ STEP_4/STEP_4/1110.py
@@ -1,11 +1,13 @@
 num=input()
-temp=num
+if int(num)<10: num='0'+num
+temp=num[:]
 temp2=0
 cycle=0
 while True:
     for i in temp:
         temp2+=int(i)
     temp=temp[len(temp)-1]+str(temp2)[len(str(temp2))-1]
+    temp2=0
     if temp==num:
         cycle+=1;break
     else: cycle+=1
