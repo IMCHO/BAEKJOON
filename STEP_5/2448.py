80b74903f4875dec1fb754b4f09b185265e8fe29
IMCHO <dlsxor21c@naver.com>
2019-01-07 12:19:20 +0900

    Complete!!!

--- STEP_5/2448.py
+++ STEP_5/2448.py
@@ -1,15 +1,16 @@
-def printStar(n1, n2):
-    temp = (5 * pow(2, n1) + pow(2, n1) - 1) // 2
-    print(' ' * temp + '*' + ' ' * (temp * 2 + 1) + '*' * n2)
-    print(' ' * (temp - 1) + '* *' + ' ' * ((temp - 1) * 2 + 1) + '* *' * n2)
-    print(' ' * (temp - 2) + '*****' + ' ' * ((temp - 2) * 2 + 1) + '*****' * n2)
-    while (n1 > 0):
-        n1 -= 1
-        n2 += 1
-        printStar(n1, n2)
+import math
 
+star=['  *   ',' * *  ','***** ']
+
+def printStar(n1):
+        for i in range(len(star)):
+            star.append(star[i]+star[i])
+            star[i]=('   '*n1+star[i]+'   '*n1)
 
 num = int(input())
-a, b = 3, 2
-c = (num // a // b)
-printStar(c, 0)
+k=int(math.log(num//3,2))
+for i in range(k):
+    printStar(pow(2,i))
+
+for i in range(len(star)):
+    print(star[i])
