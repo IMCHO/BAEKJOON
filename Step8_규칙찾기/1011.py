be307f234f1aadb09a34bafaa74d9c38bcb84244
IMCHO <dlsxor21c@naver.com>
2019-03-14 17:56:46 -0400

    규칙을 찾아야 할 것 같음!

--- Step8_규칙찾기/1011.py
+++ Step8_규칙찾기/1011.py
@@ -1,16 +1,10 @@
 for _ in range(int(input())):
     x, y = (int(i) for i in input().split())
-    maxMove = 2
-    if y - 1 < x + 1:
-        print("1")
-    elif y - 1 == x + 1:
-        print("2")
-    else:
-        remainDistance = y - 1 - (x + 1)
-        while True:
-            if remainDistance <= maxMove:
-                print(maxMove + 1)
-                break
-            else:
-                remainDistance -= maxMove
-                maxMove += 1
+    move=1
+    x+=move
+    timesOfUse=1
+    while True:
+        if x==y and move==1:
+            print(timesOfUse)
+            break
+        
