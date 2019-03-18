8b6a4df64441396ddb5e569a356b518a2391763c
IMCHO <dlsxor21c@naver.com>
2019-03-17 21:32:59 -0400

    [ing] 규칙을 찾아보자!

--- Step8_규칙찾기/1011.py
+++ Step8_규칙찾기/1011.py
@@ -1,10 +1,16 @@
 for _ in range(int(input())):
     x, y = (int(i) for i in input().split())
-    move=1
-    x+=move
-    timesOfUse=1
+    recordOfUse = [1]
+    x += recordOfUse[0]
     while True:
-        if x==y and move==1:
-            print(timesOfUse)
-            break
-        
+        if x > y:
+            x -= recordOfUse.pop()
+        elif x == y:
+            if recordOfUse[len(recordOfUse) - 1] == 1:
+                print(len(recordOfUse))
+                break
+            else:
+                x -= recordOfUse.pop()
+        move = recordOfUse[len(recordOfUse) - 1] + 1
+        x += move
+        recordOfUse.append(move)
