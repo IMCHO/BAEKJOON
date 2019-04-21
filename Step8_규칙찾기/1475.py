3962c9cde3576c6848eb8001e80202ded66eb3e3
IMCHO <dlsxor21c@naver.com>
2019-04-21 18:14:08 -0400

    Use another parameter in Max func & use another simple logic instead of if-else logic

--- Step8_규칙찾기/1475.py
+++ Step8_규칙찾기/1475.py
@@ -4,13 +4,5 @@ for num in input():
         dict['6'] = dict.get('6', 0) + 1
     else:
         dict[num] = dict.get(num, 0) + 1
-sixOrNine = dict.get('6', 0)
-dict['6'] = 0
-maxCnt = max(dict.values())
-if sixOrNine / 2 <= maxCnt:
-    print(maxCnt)
-else:
-    if sixOrNine % 2 == 0:
-        print(sixOrNine // 2)
-    else:
-        print(sixOrNine // 2 + 1)
+dict['6'] = (dict.get('6', 0) + 1) // 2
+print(max(dict.values(), default=0))
