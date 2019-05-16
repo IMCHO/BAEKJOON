f54f8cb48393a541c8f798e4c5741454bcf4496c
IMCHO <dlsxor21c@naver.com>
2019-05-15 21:20:37 -0400

    Use feature of max & min to simplify logic and my algorithm is correct, but need to reduce processing time

--- Step8_규칙찾기/6064.py
+++ Step8_규칙찾기/6064.py
@@ -1,33 +1,25 @@
 for _ in range(int(input())):
     M, N, x, y = list(map(int, input().split()))
-    bigNum, smlNum = max(M, N), min(M, N)
-    diff = bigNum - smlNum
+    bigNum, smlNum = max([M, x], [N, y]), min([M, x], [N, y])  # max/min 함수에 배열을 넣으면 첫번째 인자를 기준으로 판단한다! 같으면 다음 인자로 넘어감!
+    diff = bigNum[0] - smlNum[0]
+    diff = diff if diff != 0 else bigNum[0]
+    while diff > smlNum[0]:
+        diff -= smlNum[0]
     arrOfMatch = [diff]
     num = diff
     while True:
-        if num == smlNum:
+        if num == smlNum[0]:
             break
         num += diff
-        if num > smlNum:
-            num = num - smlNum
+        if num > smlNum[0]:
+            num = num - smlNum[0]
         arrOfMatch.append(num)
-    if bigNum == M:
-        try:
-            if y + M - x > N:
-                temp = arrOfMatch.index(y + M - x - N)
-            else:
-                temp = arrOfMatch.index(y + M - x)
-        except:
-            print(-1)
-        else:
-            print((temp + 1) * M - (M - x))
+    temp = bigNum[0] - bigNum[1] + smlNum[1]
+    while temp > smlNum[0]:
+        temp -= smlNum[0]
+    try:
+        loc = arrOfMatch.index(temp)
+    except:
+        print(-1)
     else:
-        try:
-            if x + N - y > M:
-                temp = arrOfMatch.index(x + N - y - M)
-            else:
-                temp = arrOfMatch.index(x + N - y)
-        except:
-            print(-1)
-        else:
-            print((temp + 1) * N - (N - y))
+        print((loc + 1) * bigNum[0] - (bigNum[0] - bigNum[1]))
