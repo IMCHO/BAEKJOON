65a981e54b2347213b6c1b2602f6760a41a15a42
IMCHO <dlsxor21c@naver.com>
2019-06-14 23:38:16 -0400

    Using Insertion Sort complete!

--- STEP 9_Sort/2750.py
+++ STEP 9_Sort/2750.py
@@ -1,1 +1,12 @@
-for i in sorted([int(input()) for _ in range(int(input()))]): print(i)
+# for i in sorted([int(input()) for _ in range(int(input()))]): print(i)
+
+arr = []
+for _ in range(int(input())): arr.append(int(input()))
+for i in range(1, len(arr)):
+    temp = arr[i]
+    for j in range(i - 1, -1, -1):
+        if temp < arr[j]:
+            arr[j + 1], arr[j] = arr[j], temp
+        else:
+            break
+for i in arr: print(i)
