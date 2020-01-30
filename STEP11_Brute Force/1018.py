f55ed11a6e5bfe589151185617816b4fad94f264
IMCHO <dlsxor21c@naver.com>
2020-01-30 23:06:30 +0900

    It took lots of time and has long code, but there is no choice. I can't understand the other codes that someone made.

--- STEP11_Brute Force/1018.py
+++ STEP11_Brute Force/1018.py
@@ -1,12 +1,19 @@
-def checkBoard(sample, board):
+def checkBoard(sample, board, col):
     count = 0
     for i in range(len(board)):
-        if sample[i] != board[i]: count += 1
+        for j in range(len(board[i])):
+            if sample[i][col:col + 8][j] != board[i][j]: count += 1
     return count
 
 
-board1 = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
-board2 = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
+board1 = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+          ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
+board2 = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+          ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
 
 M, N = map(int, input().split())
 sample = []
@@ -18,9 +25,8 @@ for startRow in range(M - 7):
     for startCol in range(N - 7):
         count1 = 0
         count2 = 0
-        for row in sample[startRow:startRow + 8]:
-            count1 += checkBoard(row[startCol:startCol + 8], board1)
-            count2 += checkBoard(row[startCol:startCol + 8], board2)
+        count1 += checkBoard(sample[startRow:startRow + 8], board1, startCol)
+        count2 += checkBoard(sample[startRow:startRow + 8], board2, startCol)
         counts.append(count1)
         counts.append(count2)
 print(min(counts))
