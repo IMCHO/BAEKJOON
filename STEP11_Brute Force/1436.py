fb4c8462e609771760ccd157ffcceeb274043a08
IMCHO <dlsxor21c@naver.com>
2020-02-25 18:39:11 +0900

    Complete! I didn't consider about length more than 3.

--- STEP11_Brute Force/1436.py
+++ STEP11_Brute Force/1436.py
@@ -1,11 +1,9 @@
 N = int(input())
 init = 665
 while N != 0:
-    strNum = str(init)
-    if strNum.count('6') == 3:
-        start = strNum.index('6')
-        if strNum[start:start + 3] == '666':
-            N -= 1
     init += 1
+    strNum = str(init)
+    if strNum.count('666') >= 1:
+        N -= 1
 
-print(init - 1)
+print(init)
