4d28fb0f9b92a9cb620ded15848284bd8ebf899a
IMCHO <dlsxor21c@naver.com>
2020-02-26 10:58:23 +0900

    Used Bubble Sort!

--- STEP12_Sort/2750.py
+++ STEP12_Sort/2750.py
@@ -11,22 +11,32 @@
 #             break
 # for i in arr: print(i)
 
+# arr = []
+# for _ in range(int(input())): arr.append(int(input()))
+# i = 0
+# count = 0
+# while True:
+#     try:
+#         if arr[i] > arr[i + 1]:
+#             arr[i], arr[i + 1] = arr[i + 1], arr[i]
+#         else:
+#             count += 1
+#     except:
+#         if len(arr) == 1:
+#             break
+#         else:
+#             i, count = 0, 0
+#             continue
+#     i += 1
+#     if count == len(arr) - 1: break
+# for i in arr: print(i)
+# -------------------------------------------------------------------------- Insert Sort
+
 arr = []
 for _ in range(int(input())): arr.append(int(input()))
-i = 0
-count = 0
-while True:
-    try:
-        if arr[i] > arr[i + 1]:
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        else:
-            count += 1
-    except:
-        if len(arr) == 1:
-            break
-        else:
-            i, count = 0, 0
-            continue
-    i += 1
-    if count == len(arr) - 1: break
+
+for i in range(0,len(arr)-1):
+    for j in range(0,len(arr)-1-i):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
 for i in arr: print(i)
