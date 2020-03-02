e81e316896d614b31c0461e05b44fe218840c349
IMCHO <dlsxor21c@naver.com>
2020-03-02 23:07:55 +0900

    Dividing section is done and Merge section still doing.

--- STEP12_Sort/2751.py
+++ STEP12_Sort/2751.py
@@ -1,17 +1,22 @@
 def mergeSort(array):
-    length=len(array)
-    if length!=1:
-        mid=length//2
-        left=array[0:mid]
-        right=array[mid:length]
-        mergeSort(left)
-        mergeSort(right)
-    else:
+    # print(array)
+    length = len(array)
+    if length <= 2:
         return array
+    else:
+        mid = length // 2
+        temp = []
+        left = mergeSort(array[0:mid])
+        right = mergeSort(array[mid:length])
+        tempLen = len(left) if len(left) < len(right) else len(right)
 
-def mergeSort2(left,right):
+        return temp
+        # return mergeSort(array[0:mid]) + mergeSort(array[mid:length])
+
+
+# def mergeSort2(left,right):
 
 
 arr = []
 for _ in range(int(input())): arr.append(int(input()))
-
+print(mergeSort(arr))
