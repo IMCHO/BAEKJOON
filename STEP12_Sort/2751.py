2ab508b8f928580d481e2fc351b527ac9ccc32cb
IMCHO <dlsxor21c@naver.com>
2020-03-16 20:13:42 +0900

    Used while-loop instead of nested for-loop and don't need to store 0 in array to remember starting index because I used pop() and index 0 to compare.

--- STEP12_Sort/2751.py
+++ STEP12_Sort/2751.py
@@ -1,5 +1,4 @@
 def mergeSort(array):
-    # print(array)
     length = len(array)
     if length == 1:
         return array
@@ -9,44 +8,19 @@ def mergeSort(array):
         left = mergeSort(array[0:mid])
         right = mergeSort(array[mid:length])
 
-        # print("left: {0}".format(left))
-        # print("right: {0}".format(right))
-
-        for leftValue in left:
-            if leftValue == 0: continue
-            for rightValue in right:
-                if rightValue == 0: continue
-                if leftValue < rightValue:
-                    temp.append(leftValue)
-                    left[left.index(leftValue)] = 0
-                    break
-                else:
-                    temp.append(rightValue)
-                    right[right.index(rightValue)] = 0
-
-        left = dealWithZero(left)
-        right = dealWithZero(right)
-
-        # print("after left: {0}".format(left))
-        # print("after right: {0}".format(right))
+        LIndex, RIndex = 0, 0
+        while len(left) > 0 and len(right) > 0:
+            if left[LIndex] > right[RIndex]:
+                temp.append(right.pop(RIndex))
+            else:
+                temp.append(left.pop(LIndex))
 
         if len(left) != 0:
             temp += left
         elif len(right) != 0:
             temp += right
 
-        # print("temp: {0}".format(temp))
-
         return temp
-        # return mergeSort(array[0:mid]) + mergeSort(array[mid:length])
-
-
-def dealWithZero(arr):
-    if arr.count(0) >= 1:
-        arr = set(arr)
-        arr.remove(0)
-        return list(arr)
-    return arr
 
 
 arr = []
