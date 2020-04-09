7428125a34c17062a6cd4f0a467a81075d3e3bd5
In Taek Cho <dlsxor21c@naver.com>
2020-04-10 00:01:58 +0900

    Write ideas and still don't know how to get the shortest way

---
+++ SamsungSW/13460.py
@@ -0,0 +1,9 @@
+'''
+    store a map in array
+    check if a ball can move by '+5, -5, +1,-1'
+    store a spot where ball move to in a queue
+    get a direction depending on '+5, -5, +1,-1'
+    count how many changing direction until meeting hole
+    if it is under 10, get a minimum value
+    if these are all over 10, print -1
+'''
