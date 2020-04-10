87c030d910ced45db91d9931ee67446e0f827bae
In Taek Cho <dlsxor21c@naver.com>
2020-04-10 11:25:58 +0900

    Change specification and need to learn queue in python

--- SamsungSW/13460.py
+++ SamsungSW/13460.py
@@ -1,9 +1,8 @@
 '''
-    store a map in array
-    check if a ball can move by '+5, -5, +1,-1'
-    store a spot where ball move to in a queue
-    get a direction depending on '+5, -5, +1,-1'
-    count how many changing direction until meeting hole
-    if it is under 10, get a minimum value
-    if these are all over 10, print -1
+    Make a dictionary for map by '+5, -5, +1,-1'.
+    Get a direction depending on '+5, -5, +1,-1'.
+    Count how many changing direction until meeting hole.
+    If it is under 10, get a minimum value.
+    If these are all over 10, print -1.
+    See where blue is moving to while red is moving.
 '''
