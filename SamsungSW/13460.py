18acac2ff5a20e90cfdc9b048564647ddfff5a96
In Taek Cho <dlsxor21c@naver.com>
2020-04-11 22:48:58 +0900

    Convert map to graph

--- SamsungSW/13460.py
+++ SamsungSW/13460.py
@@ -6,3 +6,23 @@
     If these are all over 10, print -1.
     See where blue is moving to while red is moving.
 '''
+row, col = map(int, input().split())
+graph = {}
+map = ""
+for _ in range(row):
+    map += input()
+map = list(map)
+for index, spot in enumerate(map):
+    if spot == "#":
+        continue
+    if spot == "." or spot == "R" or spot == "B" or spot == "O":
+        graph[index] = []
+        if map[index + 5] != "#":
+            graph[index].append(index + 5)
+        if map[index - 5] != "#":
+            graph[index].append(index - 5)
+        if map[index + 1] != "#":
+            graph[index].append(index + 1)
+        if map[index - 1] != "#":
+            graph[index].append(index - 1)
+print(graph)
