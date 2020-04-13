c772e8105b46251f9d02f9a39a588da82d88ebe1
In Taek Cho <dlsxor21c@naver.com>
2020-04-13 13:17:04 +0900

    Get indexes of red/blue/destination and make BFS function

--- SamsungSW/13460.py
+++ SamsungSW/13460.py
@@ -6,12 +6,31 @@
     If these are all over 10, print -1.
     See where blue is moving to while red is moving.
 '''
+
+
+def BFS(graph, start, end):
+    queue = [[start, [start]]]
+    result = []
+    while queue:
+        informationOfSpot = queue.pop(0)
+        if informationOfSpot[0] != end:
+            for index in graph[informationOfSpot[0]]:
+                if index not in informationOfSpot[1]:
+                    temp = informationOfSpot[1][:]
+                    temp.append(index)
+                    queue.append([index, temp])
+                if index == end:
+                    result.append(temp)
+    print(result)
+
+    # Add direction
+
+
 row, col = map(int, input().split())
 graph = {}
 map = ""
 for _ in range(row):
     map += input()
-map = list(map)
 for index, spot in enumerate(map):
     if spot == "#":
         continue
@@ -25,4 +44,8 @@ for index, spot in enumerate(map):
             graph[index].append(index + 1)
         if map[index - 1] != "#":
             graph[index].append(index - 1)
-print(graph)
+
+indexOfRed = map.index('R')
+indexOfBlue = map.index('B')
+indexOfDestination = map.index('O')
+BFS(graph, indexOfRed, indexOfDestination)
