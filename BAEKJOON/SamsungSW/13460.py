47112ff4ac43afbea74ede18dd9998eba6b71b12
In Taek Cho <dlsxor21c@naver.com>
2020-04-28 00:29:35 +0900

    Clear everything and restart to use another way

--- BAEKJOON/SamsungSW/13460.py
+++ BAEKJOON/SamsungSW/13460.py
@@ -6,53 +6,20 @@
     If these are all over 10, print -1.
     See where blue is moving to while red is moving.
 '''
-
-
-def BFS(graph, start, blue, end):
-    queue = [[[start, "X"], [start, ], 0]]
-    result1 = []
-    result2 = []
-    while queue:
-        informationOfSpot = queue.pop(0)
-        if informationOfSpot[0][0] != end:
-            for index in graph[informationOfSpot[0][0]]:
-                if index[0] not in informationOfSpot[1]:
-                    changeDirection = informationOfSpot[2]
-                    if informationOfSpot[0][1] != index[1]: changeDirection += 1
-                    temp = informationOfSpot[1][:]
-                    temp.append(index[0])
-                    queue.append([[index[0], index[1]], temp, changeDirection])
-                if index[0] == end:
-                    result1.append(temp)
-                    result2.append(changeDirection)
-    print(result1)
-    print(result2)
-
-    for index in range(len(result1)):
-        if result1[index]
-
+from collections import deque
 
 row, col = map(int, input().split())
-graph = {}
-map = ""
+map = []
 for _ in range(row):
-    map += input()
-# print(map)
-for index, spot in enumerate(map):
-    if spot == "#":
-        continue
-    if spot == "." or spot == "R" or spot == "B" or spot == "O":
-        graph[index] = []
-        if map[index + col] != "#":
-            graph[index].append([index + col, "N"])
-        if map[index - col] != "#":
-            graph[index].append([index - col, "S"])
-        if map[index + 1] != "#":
-            graph[index].append([index + 1, "E"])
-        if map[index - 1] != "#":
-            graph[index].append([index - 1, "W"])
-# print(graph)
-indexOfRed = map.index('R')
-indexOfBlue = map.index('B')
-indexOfDestination = map.index('O')
-BFS(graph, indexOfRed, indexOfBlue, indexOfDestination)
+    map.append(list(input()))
+
+xR, yR, xB, yB = 0, 0, 0, 0
+xD, yD = 0, 0
+for index, line in enumerate(map):
+    if 'R' in line:
+        xR, yR = index, line.index('R')
+    if 'B' in line:
+        xB, yB = index, line.index('B')
+    if 'O' in line:
+        xD, yD = index, line.index('O')
+# print(xR,yR,xB,yB,xD,yD)
