2d25fb8e142badf48a0c492e6263e1b00daeb017
In Taek Cho <dlsxor21c@naver.com>
2020-05-03 15:32:35 +0900

    Change the way to using Object like hash or dictionary

--- PROGRAMMERS/Hash/marathon_lv1.js
+++ PROGRAMMERS/Hash/marathon_lv1.js
@@ -1,16 +1,23 @@
 function solution(participant, completion) {
     var answer = '';
 
-    for (let index = 0; index < participant.length; index++) {
-        const element = participant[index];
-        var numInP = participant.filter(name => name == element);
-        var numInC = completion.filter(name => name == element);
-        if (numInC.length == 0) {
-            answer = element;
-            return answer;
+    var people = {};
+    participant.forEach(element => {
+        if (!people[element]) {
+            people[element] = 1;
+        } else {
+            people[element] += 1;
         }
-        if (numInC.length != numInP.length) {
-            answer = element;
+    });
+
+    completion.forEach(element => {
+        people[element] -= 1;
+    });
+
+    for (let index = 0; index < participant.length; index++) {
+        const element = people[participant[index]];
+        if (element != 0) {
+            answer = participant[index];
             return answer;
         }
     }
