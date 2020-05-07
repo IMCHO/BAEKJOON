9e6215df8b1c59f760688ea3fde7dee654d3d807
In Taek Cho <dlsxor21c@naver.com>
2020-05-08 00:56:27 +0900

    Add element to compare with weight before push, but still some cases don't pass

--- PROGRAMMERS/Stack & Queue/truck.js
+++ PROGRAMMERS/Stack & Queue/truck.js
@@ -1,34 +1,21 @@
 function solution(bridge_length, weight, truck_weights) {
     let stack = [];
-    let lastTime = 0;
+    let lastTime;
     let temp;
     truck_weights.forEach(element => {
         if (stack.length == 0) {
             stack.push([element, bridge_length + 1]);
             lastTime = bridge_length + 1;
         }
-        else if (stack.reduce((a, b) => [a[0] + b[0], 0])[0] <= weight) {
+        else if (stack.reduce((a, b) => [a[0] + b[0], 0])[0] + element <= weight) {
             stack.push([element, ++lastTime]);
+        } else {
             while (true) {
-                if (stack.reduce((a, b) => [a[0] + b[0], 0])[0] <= weight) {
-                    break;
-                } else {
-                    stack.pop();
-                    temp = stack.shift();
+                temp = stack.shift();
+                if (stack.reduce((a, b) => [a[0] + b[0], 0], [0, 0])[0] + element <= weight) {
                     stack.push([element, temp[1] + bridge_length]);
                     lastTime = temp[1] + bridge_length;
-                }
-            }
-        } else {
-            temp = stack.shift();
-            while (true) {
-                stack.push([element, temp[1] + bridge_length]);
-                if (stack.reduce((a, b) => [a[0] + b[0], 0])[0] <= weight) {
-                    lastTime = temp[1] + bridge_length;
                     break;
-                } else {
-                    stack.pop();
-                    temp = stack.shift();
                 }
             }
         }
