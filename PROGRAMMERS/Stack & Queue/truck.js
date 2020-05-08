67e30acd953af02ed5287104a5f352e8ab419a04
In Taek Cho <dlsxor21c@naver.com>
2020-05-08 16:27:12 +0900

    Solve with another way thanks to someone's answer, and need to use pop and push separately next time

--- PROGRAMMERS/Stack & Queue/truck.js
+++ PROGRAMMERS/Stack & Queue/truck.js
@@ -1,24 +1,30 @@
 function solution(bridge_length, weight, truck_weights) {
-    let stack = [];
-    let lastTime;
-    let temp;
-    truck_weights.forEach(element => {
-        if (stack.length == 0) {
-            stack.push([element, bridge_length + 1]);
-            lastTime = bridge_length + 1;
-        }
-        else if (stack.reduce((a, b) => [a[0] + b[0], 0])[0] + element <= weight) {
-            stack.push([element, ++lastTime]);
-        } else {
-            while (true) {
-                temp = stack.shift();
-                if (stack.reduce((a, b) => [a[0] + b[0], 0], [0, 0])[0] + element <= weight) {
-                    stack.push([element, temp[1] + bridge_length]);
-                    lastTime = temp[1] + bridge_length;
-                    break;
-                }
+    let time = 0;
+    let totalWeight = 0;
+    let onBridge = [];
+    while (onBridge.length != bridge_length) onBridge.push(0);
+
+    let nowTruck = truck_weights.shift();
+    onBridge.push(nowTruck);
+    onBridge.shift();
+    totalWeight = onBridge.reduce((a, b) => a + b);
+    time++;
+
+    while (totalWeight) {
+        totalWeight -= onBridge.shift();
+        nowTruck = truck_weights.shift();
+        if (nowTruck !== undefined) {
+            if (totalWeight + nowTruck <= weight) {
+                onBridge.push(nowTruck);
+                totalWeight += nowTruck;
+            } else {
+                onBridge.push(0);
+                truck_weights.unshift(nowTruck);
             }
+        } else {
+            onBridge.push(0);
         }
-    });
-    return stack.pop()[1];
+        time++;
+    }
+    return time;
 }
