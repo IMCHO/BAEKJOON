390c2615a6b674ec40c8ec2e1ca654c6803d318b
InTaek <dlsxor21c@naver.com>
2020-07-01 15:24:54 +0900

    Use permutation and make other function to check prime which is helpful

--- PROGRAMMERS/Brute-Force/findPrime.py
+++ PROGRAMMERS/Brute-Force/findPrime.py
@@ -1,10 +1,28 @@
+import itertools
+import math
+
+
+def check(n):
+    k = math.sqrt(n)
+    if n < 2:
+        return False
+
+    for i in range(2, int(k) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
 def solution(numbers):
     answer = 0
+    result = set()
+    for cnt in range(1, len(numbers) + 1):
+        temp = itertools.permutations(numbers, cnt)
+        for t in temp:
+            numStr = ''.join(t)
+            if numStr[0] != '0': result.add(numStr)
 
-    tempStr=''.join(sorted(list(numbers),reverse=True))
-    maxNum=int(tempStr)
-
-    for n in range(maxNum):
-        if ''.join(sorted(list(n),reverse=True)) in tempStr:
+    for num in result:
+        if check(int(num)): answer += 1
 
     return answer
