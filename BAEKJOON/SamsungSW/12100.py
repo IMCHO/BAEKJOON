e7188396d068aec124c61d49f7ab27a67982fb8c
InTaek <dlsxor21c@naver.com>
2020-07-05 10:04:38 +0900

    Use another way like moving index first and using checkboard to know which is merged already

--- BAEKJOON/SamsungSW/12100.py
+++ BAEKJOON/SamsungSW/12100.py
@@ -18,111 +18,34 @@ while (True):
     elif info[1] > 5:
         break
 
-    # # down
-    # temp = info[:]
-    # newBoard = copy.deepcopy(temp[0])
-    # for y in range(N):
-    #     for x in range(N - 1, 0, -1):
-    #         i = 0
-    #         while True:
-    #             # Case 1
-    #             if newBoard[x][y] == 0:
-    #                 for tempX in range(x, 0, -1):
-    #                     newBoard[tempX][y] = newBoard[tempX - 1][y]
-    #                 newBoard[0][y] = 0
-    #                 i += 1
-    #                 if i == N: break
-    #             # Case 2
-    #             elif newBoard[x - 1][y] == 0:
-    #                 for tempX in range(x - 1, 0, -1):
-    #                     newBoard[tempX][y] = newBoard[tempX - 1][y]
-    #                 newBoard[0][y] = 0
-    #                 break
-    #             # Case 3
-    #             # Put them down after merge
-    #             elif newBoard[x][y] == newBoard[x - 1][y]:
-    #                 newBoard[x][y] *= 2
-    #                 newBoard[x - 1][y] = 0
-    #
-    #                 for tempX in range(x - 1, 0, -1):
-    #                     newBoard[tempX][y] = newBoard[tempX - 1][y]
-    #                 newBoard[0][y] = 0
-    #                 break
-    #             # Case 4
-    #             elif newBoard[x][y] != newBoard[x - 1][y]:
-    #                 break
-    #
-    # boards.append([newBoard, temp[1] + 1])
-    #
-    # # up
-    # temp = info[:]
-    # newBoard = copy.deepcopy(temp[0])
-    # for y in range(N):
-    #     for x in range(0, N - 1):
-    #         i = 0
-    #         while True:
-    #             # Case 1
-    #             if newBoard[x][y] == 0:
-    #                 for tempX in range(x, N - 1):
-    #                     newBoard[tempX][y] = newBoard[tempX + 1][y]
-    #                 newBoard[N - 1][y] = 0
-    #                 i += 1
-    #                 if i == N: break
-    #             # Case 2
-    #             elif newBoard[x + 1][y] == 0:
-    #                 for tempX in range(x + 1, N - 1):
-    #                     newBoard[tempX][y] = newBoard[tempX + 1][y]
-    #                 newBoard[N - 1][y] = 0
-    #                 break
-    #             # Case 3
-    #             # Put them down after merge
-    #             elif newBoard[x][y] == newBoard[x + 1][y]:
-    #                 newBoard[x][y] *= 2
-    #                 newBoard[x + 1][y] = 0
-    #
-    #                 for tempX in range(x + 1, N - 1):
-    #                     newBoard[tempX][y] = newBoard[tempX + 1][y]
-    #                 newBoard[N - 1][y] = 0
-    #                 break
-    #             # Case 4
-    #             elif newBoard[x][y] != newBoard[x + 1][y]:
-    #                 break
-    #
-    # boards.append([newBoard, temp[1] + 1])
-
-    # left
+    # down
     temp = info[:]
     newBoard = copy.deepcopy(temp[0])
-    for x in range(N):
-        for y in range(N - 1):
-            i = 0
-            while True:
-                # Case 1
-                if newBoard[x][y] == 0:
-                    for tempY in range(y, N - 1):
-                        newBoard[x][tempY] = newBoard[x][tempY + 1]
-                    newBoard[x][N - 1] = 0
-                    i += 1
-                    if i == N: break
-                # Case 2
-                elif newBoard[x][y + 1] == 0:
-                    for tempY in range(y + 1, N - 1):
-                        newBoard[x][tempY] = newBoard[x][tempY]
-                    newBoard[x][N - 1] = 0
-                    break
-                # Case 3
-                # Put them down after merge
-                elif newBoard[x][y] == newBoard[x][y + 1]:
-                    newBoard[x][y] *= 2
-                    newBoard[x][y + 1] = 0
+    checkBoard = [[True] * N for _ in range(N)]
 
-                    for tempY in range(y + 1, N - 1):
-                        newBoard[x][tempY] = newBoard[x][tempY]
-                    newBoard[x][N - 1] = 0
+    for y in range(N):
+        for x in range(N - 1, -1, -1):
+            if newBoard[x][y] == 0: continue
+            value = newBoard[x][y]
+            newBoard[x][y] = 0
+            xMoving = x
+            while True:
+                xMoving += 1
+                if xMoving < 0 or xMoving > N - 1:
+                    xMoving -= 1
                     break
-                # Case 4
-                elif newBoard[x][y] != newBoard[x][y + 1]:
+                if newBoard[xMoving][y] == 0:
+                    continue
+                elif newBoard[xMoving][y] != value:
+                    xMoving -= 1
                     break
+                else:
+                    if checkBoard[xMoving][y]:
+                        checkBoard[xMoving][y] = False
+                    else:
+                        xMoving -= 1
+                    break
+            newBoard[xMoving][y] += value
 
     boards.append([newBoard, temp[1] + 1])
 
