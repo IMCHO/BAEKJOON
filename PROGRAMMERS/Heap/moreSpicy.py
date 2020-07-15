5f6fceae77a374fb94121e665de49d9c0e75bf83
InTaek <dlsxor21c@naver.com>
2020-07-15 14:07:02 +0900

    Sort after add new value, but still same

--- PROGRAMMERS/Heap/moreSpicy.py
+++ PROGRAMMERS/Heap/moreSpicy.py
@@ -1,11 +1,11 @@
 def solution(scoville, K):
     answer = 0
 
-    for i in range(len(scoville) // 2 - 1, -1, -1):
-        heapify(scoville, i, len(scoville))
-
     while len(scoville) != 1:
         if len(list(filter(lambda x: x < K, scoville))) == 0: break
+
+        for i in range(len(scoville) // 2 - 1, -1, -1):
+            heapify(scoville, i, len(scoville))
 
         scoville[0], scoville[len(scoville) - 1] = scoville[len(scoville) - 1], scoville[0]
         s1 = scoville.pop()
@@ -16,6 +16,7 @@ def solution(scoville, K):
         heapify(scoville, 0, len(scoville))
 
         scoville.append(s1 + s2 * 2)
+
         answer += 1
 
     if scoville[0] < K: return -1
