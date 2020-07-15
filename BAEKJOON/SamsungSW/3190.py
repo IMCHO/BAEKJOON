38fa1b46ec04099e484c469166f1f9990db28241
InTaek <dlsxor21c@naver.com>
2020-07-15 12:34:30 +0900

    Make apple delete after eating it

--- BAEKJOON/SamsungSW/3190.py
+++ BAEKJOON/SamsungSW/3190.py
@@ -11,11 +11,6 @@ map = [[0] * N for _ in range(N)]
 for apple in apples:
     map[apple[0] - 1][apple[1] - 1] = 1
 
-# for i in range(N):
-#     for j in range(N):
-#         print(map[i][j], end=' ')
-#     print('\n')
-
 snake = deque([[0, 0]])
 time = 0
 whichDir = 2
@@ -35,23 +30,18 @@ while 1:
             break
 
     newHead = [head[0] + offsetOfDir[whichDir][0], head[1] + offsetOfDir[whichDir][1]]
-    print(newHead[0], newHead[1])
-    print('\n')
-    for d in snake:
-        print(d[0], d[1])
-    print('\n')
 
     if newHead[0] >= N or newHead[0] < 0 or newHead[1] >= N or newHead[1] < 0:
-        print("Collapse cuz of board", newHead[0], newHead[1])
         break
 
     if newHead in snake:
-        print("Collapse cuz of snake")
         break
 
     snake.appendleft(newHead)
 
     if map[newHead[0]][newHead[1]] != 1:
         snake.pop()
+    else:
+        map[newHead[0]][newHead[1]] = 0
 
 print(time)
