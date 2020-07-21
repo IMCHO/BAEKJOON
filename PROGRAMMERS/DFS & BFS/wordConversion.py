f44b1e2dfa9187861452d5bbc542f4bd0023441f
InTaek <dlsxor21c@naver.com>
2020-07-21 12:12:36 +0900

    Put a complete code, but some cases doesn't pass

--- PROGRAMMERS/DFS & BFS/wordConversion.py
+++ PROGRAMMERS/DFS & BFS/wordConversion.py
@@ -1,30 +1,42 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+from copy import deepcopy
 
 
 def solution(begin, target, words):
-    answer = 0
     if target not in words: return 0
 
     wordMap = defaultdict(list)
 
     for word in words:
-        cnt = 0
-        for c in begin:
-            if c in word: cnt += 1
-        if cnt == 2: wordMap[begin].append(word)
+        makeMap(wordMap, begin, word)
 
     for word in words:
         for target in words:
             if word == target: continue
-            cnt = 0
-            for c in target:
-                if c in word: cnt += 1
-            if cnt == 2: wordMap[word].append(target)
+            makeMap(wordMap, word, target)
 
-    # for word in wordMap:
-    #     print(word, end=' : ')
-    #     for i in wordMap[word]:
-    #         print(i, end=' ')
-    #     print('\n')
+    deq = deque([[begin, []]])
 
-    return answer
+    while deq:
+        info = deq.popleft()
+
+        for next in wordMap[info[0]]:
+            if next in info[1]: continue
+            if next == target: return len(info[1]) + 1
+
+            newRoute = deepcopy(info[1])
+            newRoute.append(info[0])
+            newInfo = [next, newRoute]
+            deq.append(newInfo)
+
+            print("now : " + next)
+            print("before : " + ' '.join(newRoute))
+
+    return 0
+
+
+def makeMap(wordMap, index, word):
+    cnt = 0
+    for c in index:
+        if c in word: cnt += 1
+    if cnt == 2: wordMap[index].append(word)
