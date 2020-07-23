d9aa6b332bed27c957cac63f87a008fdc184330a
InTaek <dlsxor21c@naver.com>
2020-07-23 14:25:22 +0900

    Change some code with same logic, but still doesn't pass

--- PROGRAMMERS/DFS & BFS/wordConversion.py
+++ PROGRAMMERS/DFS & BFS/wordConversion.py
@@ -15,28 +15,33 @@ def solution(begin, target, words):
             if word == target: continue
             makeMap(wordMap, word, target)
 
-    deq = deque([[begin, []]])
+    deq = deque([[begin, [begin]]])
+
+    result = []
 
     while deq:
         info = deq.popleft()
 
         for next in wordMap[info[0]]:
             if next in info[1]: continue
-            if next == target: return len(info[1]) + 1
+            if next == target:
+                # return len(info[1])
+                result.append(len(info[1]))
+                continue
 
             newRoute = deepcopy(info[1])
-            newRoute.append(info[0])
+            newRoute.append(next)
             newInfo = [next, newRoute]
             deq.append(newInfo)
 
-            print("now : " + next)
-            print("before : " + ' '.join(newRoute))
+            # print("now : " + next)
+            # print("before : " + ' '.join(newRoute))
 
-    return 0
+    return min(result) if len(result) != 0 else 0
 
 
 def makeMap(wordMap, index, word):
     cnt = 0
     for c in index:
         if c in word: cnt += 1
-    if cnt == 2: wordMap[index].append(word)
+    if cnt == len(word) - 1: wordMap[index].append(word)
