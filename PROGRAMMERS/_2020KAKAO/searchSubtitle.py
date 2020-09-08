4f88a3543440c015b85944acbe1f3dee11d529d6
InTaek <dlsxor21c@naver.com>
2020-09-08 13:54:55 +0900

    Use dictionary not to double-check in regex code, and try Trie data structure

--- PROGRAMMERS/_2020KAKAO/searchSubtitle.py
+++ PROGRAMMERS/_2020KAKAO/searchSubtitle.py
@@ -2,15 +2,48 @@ import re
 
 
 def solution(words, queries):
-    answer = []
+    # answer = []
+    # d = {}
+    # for query in queries:
+    #     if query not in d.keys():
+    #         cntOfQMark = query.count("?")
+    #         if query.startswith("?"):
+    #             pattern = "[a-z]" + "{" + str(cntOfQMark) + "}" + query[query.rfind("?") + 1:]
+    #         else:
+    #             pattern = query[0:query.find("?")] + "[a-z]" + "{" + str(cntOfQMark) + "}"
+    #         regex = re.compile(pattern)
+    #         result = regex.findall(" ".join(filter(lambda x: len(x) == len(query), words)))
+    #         d[query] = len(set(result))
+    #
+    #     answer.append(d[query])
+    # return answer
+    trie = Trie()
+    for word in words:
+        trie.addWord(word)
+    print(trie)
 
-    for query in queries:
-        cntOfQMark = query.count("?")
-        if query.startswith("?"):
-            pattern = "[a-z]" + "{" + str(cntOfQMark) + "}" + query[query.rfind("?") + 1:]
-        else:
-            pattern = query[0:query.find("?")] + "[a-z]" + "{" + str(cntOfQMark) + "}"
-        regex = re.compile(pattern)
-        result = regex.findall(" ".join(filter(lambda x: len(x) == len(query), words)))
-        answer.append(len(set(result)))
-    return answer
+
+class Node:
+    def __init__(self):
+        self.value = ""
+        self.next = dict()
+
+
+class Trie:
+    def __init__(self):
+        self.head = Node()
+        self.cur = self.head
+
+    def addWord(self, word):
+        for c in word:
+            if c not in self.cur.next.keys():
+                newNode = Node()
+                newNode.value = c
+                self.cur.next[c] = newNode
+            self.cur = self.cur.next[c]
+
+        self.cur = self.head
+
+    def searchWord(self, word):
+        for c in word:
+            if c == "?":
