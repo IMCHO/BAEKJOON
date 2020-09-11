ede85f47c3b11a998b938aa017c5e7a1ee9178cb
InTaek <dlsxor21c@naver.com>
2020-09-11 22:39:08 +0900

    Check all frames all the time whenever append or remove

--- PROGRAMMERS/_2020KAKAO/Pillar&Beam.py
+++ PROGRAMMERS/_2020KAKAO/Pillar&Beam.py
@@ -1,42 +1,38 @@
 def solution(n, build_frame):
-    answer = [[]]
-    points = []
+    frames = []
 
     for frame in build_frame:
         x, y, a, b = frame
-        # set pillar
-        if a == 0 and b == 1:
-            if y == 0 \
-                    or [x, y - 1, 0] in points \
-                    or ([x - 1, y, 1] in points and [x, y, 1] not in points) \
-                    or ([x - 1, y, 1] not in points and [x, y, 1] in points):
-                points.append([x, y, 0])
-        # set beam
-        elif a == 1 and b == 1:
-            if [x, y, 0] in points \
-                    or [x + 1, y, 0] in points \
-                    or ([x - 1, y, 1] in points and [x + 1, y, 1] in points):
-                points.append([x, y, 1])
-        # remove pillar
-        elif a == 0 and b == 0:
-            if [x, y + 1, 1] in points:
-                if ([x - 1, y + 1, 1] in points and [x + 1, y + 1, 1] in points) or [x + 1, y, 0] in points:
-                    points.remove([x, y, 0])
-            elif [x - 1, y + 1, 1] in points:
-                if ([x - 2, y + 1, 1] in points and [x, y + 1, 1] in points) or [x - 1, y, 0] in points:
-                    points.remove([x, y, 0])
-            elif [x, y + 1, 0] in points:
+        if a == 0 and b == 0:
+            frames.remove([x, y, 0])
+            if not check(frames): frames.append([x, y, 0])
+        elif a == 0 and b == 1:
+            frames.append([x, y, 0])
+            if not check(frames[::-1]): frames.remove([x, y, 0])
+        elif a == 1 and b == 0:
+            frames.remove([x, y, 1])
+            if not check(frames): frames.append([x, y, 1])
+        else:
+            frames.append([x, y, 1])
+            if not check(frames[::-1]): frames.remove([x, y, 1])
+
+    answer = sorted(frames, key=lambda x: (x[0], x[1], x[2]))
+    return answer
+
+
+def check(frames):
+    for frame in frames:
+        x, y, a = frame
+
+        if a == 0:
+            if y == 0 or [x - 1, y, 1] in frames or [x, y, 1] in frames or [x, y - 1, 0] in frames:
                 continue
             else:
-                points.remove([x, y, 0])
-
-        elif a == 1 and b == 0:
-            if ([x, y, 0] in points and ([x - 1, y, 1] not in points or [x, y - 1, 0] not in points)) \
-                    or ([x + 1, y, 0] in points and ([x + 1, y, 1] not in points or [x + 1, y - 1, 0] not in points)) \
-                    or ([x + 1, y, 1] in points and [x + 1, y - 1, 0] not in points and [x + 2, y - 1, 0] not in points) \
-                    or ([x - 1, y, 1] in points and [x, y - 1, 0] not in points and [x - 1, y - 1, 0] not in points):
+                return False
+        else:
+            if [x, y - 1, 0] in frames or [x + 1, y - 1, 0] in frames or (
+                    [x - 1, y, 1] in frames and [x + 1, y, 1] in frames):
                 continue
-            points.remove([x, y, 1])
-
-    answer = sorted(points, key=lambda x: (x[0], x[1], x[2]))
-    return answer
+            else:
+                return False
+    return True
