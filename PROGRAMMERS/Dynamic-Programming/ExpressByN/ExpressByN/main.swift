7579bfe6534300ef3e9958348c859e01fd77c38f
InTaek <dlsxor21c@naver.com>
2020-09-19 23:09:28 +0900

    Fix some issue, but still doesn't work

--- PROGRAMMERS/Dynamic-Programming/ExpressByN/ExpressByN/main.swift
+++ PROGRAMMERS/Dynamic-Programming/ExpressByN/ExpressByN/main.swift
@@ -8,86 +8,54 @@
 import Foundation
 
 func solution(_ N:Int, _ number:Int) -> Int {
-    let cntOfNumber = String(number).count
     var initial = [Int]()
-    var arr = [[Int]]()
+    var BFS = [Array<Int>]()
     
-    for i in 1...cntOfNumber {
-        let temp = Int(String(repeating: String(N), count: i))!
-        if temp == number {
-            return String(temp).count
-        }
+    for cnt in 1...7 {
+        let temp = Int(String(repeating: String(N), count: cnt))!
         initial.append(temp)
-        arr.append([temp, String(temp).count])
+        BFS.append([temp, String(temp).count])
     }
     
-    var startIndex = 0
-    var endIndex = arr.count
-    
-    while true {
-        var listForExtension = [[Int]]()
-        for target in arr[startIndex..<endIndex] {
-//            print(target)
-            let num = target[0]
-            let cnt = target[1]
+    while BFS.count != 0 {
+        let target = BFS.removeFirst()
+        if target[1] > 8 {
+             return -1
+        }
+        
+        for offset in initial {
+            let cnt = String(offset).count + target[1]
+            let newValue1 = target[0] + offset
+            let newValue2 = target[0] - offset
+            let newValue3 = target[0] / offset
+            let newValue4 = target[0] * offset
             
-            for offset in initial {
-                if check(num + offset, with: number) {
-                    if checkCount(cnt + String(offset).count) {
-                        return cnt + String(offset).count
-                    } else {
-                        return -1
-                    }
+            if newValue1 == number || newValue2 == number || newValue3 == number || newValue4 == number {
+                if cnt > 8 {
+                    return -1
+                } else {
+                    return cnt
                 }
-                if check(num - offset, with: number) {
-                    if checkCount(cnt + String(offset).count) {
-                        return cnt + String(offset).count
-                    } else {
-                        return -1
-                    }
-                }
-                if check(num / offset, with: number) {
-                    if checkCount(cnt + String(offset).count) {
-                        return cnt + String(offset).count
-                    } else {
-                        return -1
-                    }
-                }
-                if check(num * offset, with: number) {
-                    if checkCount(cnt + String(offset).count) {
-                        return cnt + String(offset).count
-                    } else {
-                        return -1
-                    }
-                }
-                    listForExtension.append([num + offset, cnt + String(offset).count])
-                    listForExtension.append([num - offset, cnt + String(offset).count])
-                    listForExtension.append([num * offset, cnt + String(offset).count])
-                    listForExtension.append([num / offset, cnt + String(offset).count])
+            }
+            
+            if newValue1 != number {
+                BFS.append([newValue1, cnt])
+            }
+            if newValue2 != number {
+                BFS.append([newValue2, cnt])
+            }
+            if newValue3 != number {
+                BFS.append([newValue3, cnt])
+            }
+            if newValue4 != number {
+                BFS.append([newValue4, cnt])
             }
         }
-        startIndex = endIndex
-        endIndex = endIndex + listForExtension.count
-        arr += listForExtension
-//        print(listForExtension)
+        BFS.sort {
+            $0[1] < $1[1]
+        }
     }
+    return -1
 }
-
-func check(_ value: Int, with answer: Int) -> Bool {
-    if value == answer {
-        return true
-    } else {
-        return false
-    }
-}
-
-func checkCount(_ value: Int) -> Bool {
-    if value <= 8 {
-        return true
-    } else {
-        return false
-    }
-}
-
 
 print(solution(5, 31168))
