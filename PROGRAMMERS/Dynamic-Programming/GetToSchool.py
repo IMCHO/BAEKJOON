68e012647cff9586615ffd56e34bed0e3e54a40f
InTaek <dlsxor21c@naver.com>
2020-09-24 15:28:50 +0900

    Use DP after making a double array map

--- PROGRAMMERS/Dynamic-Programming/GetToSchool.py
+++ PROGRAMMERS/Dynamic-Programming/GetToSchool.py
@@ -1,21 +1,24 @@
 def solution(m, n, puddles):
-    routes = [[0, 0]]
-    copiedPuddles = list(map(lambda x: [x[0] - 1, x[1] - 1], puddles))
+    wholeMap = [[0 for _ in range(m)] for _ in range(n)]
+    copiedPuddles = list(map(lambda arr: [arr[1] - 1, arr[0] - 1], puddles))
+    wholeMap[0][0] = 1
+    for x, line in enumerate(wholeMap):
+        for y, value in enumerate(line):
+            upIndex = [x - 1, y]
+            leftIndex = [x, y - 1]
+            newValue = value
 
-    while True:
-        routeForExtension = []
-        for route in routes:
-            down = [route[0], route[1] + 1]
-            right = [route[0] + 1, route[1]]
+            if [x, y] in copiedPuddles: continue
+            if upIndex[0] >= 0:
+                newValue += wholeMap[upIndex[0]][upIndex[1]]
+            if leftIndex[1] >= 0:
+                newValue += wholeMap[leftIndex[0]][leftIndex[1]]
 
-            if down[1] < n and down not in copiedPuddles:
-                routeForExtension.append(down)
-            if right[0] < m and right not in copiedPuddles:
-                routeForExtension.append(right)
+            wholeMap[x][y] = newValue
+            # print(x,y)
+            # print(wholeMap[x][y])
 
-        if [m - 1, n - 1] in routeForExtension:
-            break
-        else:
-            routes = routeForExtension
+    return wholeMap[n - 1][m - 1] % 1000000007
 
-    return routeForExtension.count([m - 1, n - 1]) % 1000000007
+
+# print(solution(4, 3, [[2, 2]]))
