ad613ab0d533c51d5bb62aa368b08dcf2366f2b7
InTaek <dlsxor21c@naver.com>
2020-10-18 15:47:13 +0900

    Get just one case turns out time out

--- PROGRAMMERS/ETC/LongestPalindrome/LongestPalindrome/main.swift
+++ PROGRAMMERS/ETC/LongestPalindrome/LongestPalindrome/main.swift
@@ -10,21 +10,30 @@ import Foundation
 func solution(_ s:String) -> Int {
     var maximum = 0
     
-    for (index, _) in s.enumerated() {
-//        print(index,value)
-        let lenOfFront = s[s.startIndex..<s.index(s.startIndex, offsetBy: index)].count
-        let lenOfBack = s[s.index(s.startIndex, offsetBy: index + 1)...].count
-//        print(lenOfFront,lenOfBack)
-        let targetLen = min(lenOfFront, lenOfBack)
-
-        for n in stride(from: targetLen, through: 0, by: -1) {
-            let original = s[s.index(s.startIndex, offsetBy: index - n)...s.index(s.startIndex, offsetBy: index + n)]
-//            print(original)
-            if original == String(original.reversed()) {
-                maximum = max(maximum, original.count)
+//    for (index, _) in s.enumerated() {
+////        print(index,value)
+//        let lenOfFront = s[s.startIndex..<s.index(s.startIndex, offsetBy: index)].count
+//        let lenOfBack = s[s.index(s.startIndex, offsetBy: index + 1)...].count
+////        print(lenOfFront,lenOfBack)
+//        let targetLen = min(lenOfFront, lenOfBack)
+//
+//        for n in stride(from: targetLen, through: 0, by: -1) {
+//            let original = s[s.index(s.startIndex, offsetBy: index - n)...s.index(s.startIndex, offsetBy: index + n)]
+////            print(original)
+//            if original == String(original.reversed()) {
+//                maximum = max(maximum, original.count)
+//            }
+//        }
+//    }
+    
+    for n in stride(from: s.count, through: 1, by: -1) {
+        for startPoint in 0...s.count - n {
+            let string = s[s.index(s.startIndex, offsetBy: startPoint)..<s.index(s.index(s.startIndex, offsetBy: startPoint), offsetBy: n)]
+//            print(string)
+            if string == String(string.reversed()) {
+                return string.count
             }
         }
     }
-    
     return maximum
 }
